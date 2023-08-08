cdb018d76e08a73bab2b4dc140b046c4eb85d9a7
‘wangyuan’ <2682220169@qq.com>
2023-08-08 17:24:49 +0800

    N 皇后问题

---
+++ Arithmetic/HDOJ/2553.cpp
@@ -0,0 +1,60 @@
+/**
+ * @BelongsProject: HDOJ
+ * @Author: yuan wang
+ * @CreateTime: 2023-08-08  15:36
+ * @Description: TODO
+ * @Version: 1.0
+ */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int N;
+int queenPos[100];
+int sum;
+
+void NQueen(int k);
+
+/**
+ * @description: N 皇后问题
+ * @author: yuan wang
+ * @date: 2023/08/08 15:36
+ * @param: null
+ * @return: null
+ */
+int main() {
+    while (cin >> N) {
+        sum = 0;
+        NQueen(0);
+        cout << sum << endl;
+    }
+    return 0;
+}
+
+/**
+ * 在0~k-1行皇后已经摆好的情况下，摆第k行及其后的皇后
+ * @param k
+ */
+void NQueen(int k) {
+    int i;
+    //N个皇后已经摆好
+    if (k == N) {
+        sum ++;
+        return;
+    }
+    //逐个尝试第k个皇后的位置
+    for (i = 0; i < N; ++i) {
+        int j;
+        for (j = 0; j < k; ++j) {
+            //和已经摆好的k个皇后的位置比较，即验证同一列以及斜着不能吃到，注意斜着不能被吃即表示行列差的绝对值不相等
+            if (queenPos[j] == i || abs(queenPos[j] - i) == abs(k - j)) {
+                //冲突，测试下个位置
+                break;
+            }
+        }
+        if (j == k) {
+            queenPos[k] = i;
+            NQueen(k + 1);
+        }
+    }
+}
