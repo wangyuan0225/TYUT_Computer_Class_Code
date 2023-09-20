5d2b581df58a634e69eb3f1cd9f0b139242e12aa
‘wangyuan’ <2682220169@qq.com>
2023-09-20 21:45:30 +0800

    更新

--- CCF/test.cpp
+++ CCF/test.cpp
@@ -1,25 +1,20 @@
-#include <iostream>
-
-void generateCombinations(int n) {
-    for (int i = 0; i < (1 << n); i++) {
-        std::cout << "组合 " << i << ": ";
-        for (int j = 0; j < n; j++) {
-            if (i & (1 << j)) {
-                std::cout << "选 ";
-            } else {
-                std::cout << "不选 ";
-            }
-        }
-        std::cout << std::endl;
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m,k,t_pl,t_re;
+    cin>>n>>m>>k;
+    int t_fd[200005];
+    int t_do[m];
+    memset(t_fd,0,sizeof(t_fd));
+    for(int i = 0;i < n;i++){
+        cin>>t_pl>>t_re;
+        for(int j = (t_pl>=t_re)?(t_pl-t_re+1):1;j <= t_pl;j++)
+            t_fd[j]++;
     }
-}
-
-int main() {
-    int n;
-    std::cout << "请输入物品数量n：";
-    std::cin >> n;
-
-    generateCombinations(n);
-
+    for(int i = 0;i < m;i++)
+        cin>>t_do[i];
+    for(int i = 0;i < m;i++)
+        if(i != m-1) cout<<t_fd[t_do[i]+k]<<endl;
+    cout<<t_fd[t_do[m-1]+k];
     return 0;
 }
